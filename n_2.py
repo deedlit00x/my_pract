e353181383fbe06c4fff472970f17b5d7e3c89a4
deedlit00x <chegnoval@gmail.com>
2022-01-12 00:23:46 +0300

    изменено:      n_2.py 	новый файл:    n_3.py

--- n_2.py
+++ n_2.py
@@ -42,3 +42,56 @@
 #     return f'{result}'
 
 # print(join_numbers_from_range(5, 10))
+
+# def encrypt(text):
+#     i = 0
+#     result = ""
+#     while i < len(text):
+#         nextChar = "" if (i + 1 >= len(text)) else text[i + 1]
+#         result = result + nextChar + text[i]
+#         i += 2
+#     return result
+
+# print(encrypt('text'))
+
+# def reverse(string):
+#     result = ''
+#     for char in string:
+#         result = char + result
+#     return result
+
+# reverse('go!')  # '!og'
+
+# def filter_string(text, char):
+#     upper_char = text.replace(char.upper(), '')
+#     lower_char = upper_char.replace(char.lower(), '')
+#     result = " ".join(lower_char.split())
+#     return result
+
+# print(filter_string('I look back if I am lost', 'o'))
+
+# my_list = [12, 3, 456, 78]
+# result = ''
+# for el in my_list:
+#     result += ''.join(str(el))
+
+# print(result)
+
+# def list_compare(list_1, list_2):
+#     if len(list_1) == len(list_2):
+#         if list_1 == list_2:
+#             print('yes')
+#         else:
+#             print('no')
+#         # i = 0
+#         # while i < len(list_1):
+#         #     for el in list_1:
+#         #         if el in list_2:
+#         #             list_2.remove(el)
+#         #             print('yes')
+#         #         else:
+#         #             break
+#         #     i += 1
+
+
+# list_compare([12, 3, 456, 78], [12, 3, 456, 78])
