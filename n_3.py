1ad610f7eb04bf11180ed2503f3fef43074dc608
deedlit00x <chegnoval@gmail.com>
2022-01-13 18:31:32 +0300

    изменено:      n_3.py

---
+++ n_3.py
@@ -0,0 +1,24 @@
+
+# def get_colors():
+#     def rgb(red=0, green=0, blue=0):
+#         return 'rgb({}, {}, {})'.format(red, green, blue)
+#     return {
+#         'red': rgb(red=255),
+#         'green': rgb(green=255),
+#         'blue': rgb(blue=255),
+#     }
+
+# d = {'a': 1, 'b': None, 2: 4}
+
+# def updated(dict, **kwargs):
+#     tmp_dict = kwargs
+#     tmp = dict.copy()
+#     tmp.update(tmp_dict)
+#     return tmp
+
+# print(updated(d, a=2, b=True, c=None))
+
+# def call_twice(func, *args, **kwargs):
+#     first_f = func(*args, **kwargs)
+#     second_f = func(*args, **kwargs)
+#     return first_f, second_f
